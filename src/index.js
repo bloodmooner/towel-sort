8a7e406fd1e72bae693285ae638246097917393c
Sergey Goncharuk <fleshed914@gmail.com>
2021-08-29 10:30:41 +0300

    fix: if condition to return empty array

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@
 
 
 
-
-
 module.exports = function towelSort (matrix) {
     let sortedMatrix = [];
+
+    if(matrix === undefined) return [];
 
     if(matrix.length !== 0) {
         for(let i = 0; i < matrix.length; i++) {
